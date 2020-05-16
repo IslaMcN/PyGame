cd3215a5b8d44f934a8ddbdfec7b25c70e072ae3
Isla <fallenacademy@gmail.com>
2020-05-16 16:11:47 -0700

    made some wonky trees and a creepy smile

---
+++ smile.py
@@ -0,0 +1,37 @@
+import arcade
+# Set screen size
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+# Open the window and set the title and dimensions
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
+
+arcade.set_background_color(arcade.color.WHITE)
+# Starts render process. MUST BE DONE BEFORE ANY DRAWING COMMANDS
+arcade.start_render()
+# Draw face
+x = 300
+y = 300
+radius = 200
+arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
+# Draw right eye
+x = 370
+y = 350
+radius = 20
+arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+# Draw left eye
+x = 230
+y = 350
+radius = 20
+arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+# Draw the smile
+x = 300
+y = 280
+width = 120
+height = 100
+start_angle = 190
+end_angle = 350
+arcade.draw_arc_outline(x, y, width, height, arcade.color.BLACK, start_angle, end_angle)
+# Finish
+arcade.finish_render()
+# Keep window open until user hits close
+arcade.run()
