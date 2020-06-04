b13ebe0cb396f8403c67594bab1a4edde93ea0ea
Isla <fallenacademy@gmail.com>
2020-06-04 14:22:18 -0700

    Fixed birds

--- trees.py
+++ trees.py
@@ -9,8 +9,8 @@ def draw_background():
     arcade.draw_lrtb_rectangle_filled(0, screen_width, screen_height /3, 0, arcade.color.DARK_SPRING_GREEN)
 
 def draw_bird(x,y):
-    arcade.draw_arc_outline(x, y, 20, 20, arcade.color.BLACK, 0, 90)
-    arcade.draw_arc_outline(x+40,y,20,20,arcade.color.BLACK, 90, 180)
+    arcade.draw_arc_outline(x, y, 20, 22, arcade.color.BLACK, 20, 90, 4)
+    arcade.draw_arc_outline(x+20,y,20,22,arcade.color.BLACK, 90, 180, 4)
 
 def draw_pine_tree(x,y):
     arcade.draw_triangle_filled(x + 40, y, x, y - 100, x + 80, y - 100, arcade.color.DARK_GREEN)
