8a6987ec8fc9f734243a4d1138774424a488dcfe
Isla <fallenacademy@gmail.com>
2020-06-11 13:10:26 -0700

    On draw complete

--- myGame.py
+++ myGame.py
@@ -9,17 +9,30 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.AMAZON)
     
     def setup(self):
-        # Set up game here
         self.player_list = arcade.SpriteList()
-        self.crystal_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
 
         self.score = 0
+
+        self.player_sprite = arcade.Sprite("sprites/maincharacter/1 Woodcutter/Woodcutter_idle.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+        for i in range(COIN_COUNT):
+            coin = arcade.Sprite("sprites/items/PNG/shiny/1.png", SPRITE_SCALING_COIN)
+
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+            self.coin_list.append(coin)
 
 
 
     def on_draw(self):
-        # Drawings here
-        pass
+        arcade.start_render()
+        self.coin_list.draw()
+        self.player_list()
     
     def update(self, delta_time):
         # Logic to move and game logic
